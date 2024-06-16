3f7d39e3b8e657d60c7d7ab25bcec45cc9fcaaaa
Logan Lucas <logan.lucas83@gmail.com>
2024-06-17 00:15:16 +0200

    remotely change device name

--- ble-server.ino
+++ ble-server.ino
@@ -3,9 +3,21 @@
 
 #define DEFAULT_DEVICE_NAME "Cucklord V3"
 
-#define SERVICE_UUID        "1df96fce-7f73-4dfe-b126-89dfe643eeac"
-#define CHARACTERISTIC_UUID "df449700-a753-4a1c-953a-d561c4848045"
-#define DESCRIPTOR_UUID     "3c462cba-886d-41f5-8fe6-37801a5bf590"
+#define SERVICE_UUID                 "1df96fce-7f73-4dfe-b126-89dfe643eeac"
+#define NAME_CHARACTERISTIC_UUID     "d3139fcc-c344-45c4-a370-e32af8de79c0"
+#define NAME_DESCRIPTOR_UUID         "69344e7f-9d3a-465e-aa34-3cc2f5dbdb76"
+#define DISTANCE_CHARACTERISTIC_UUID "df449700-a753-4a1c-953a-d561c4848045"
+#define DISTANCE_DESCRIPTOR_UUID     "3c462cba-886d-41f5-8fe6-37801a5bf590"
+
+class NameCallback : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic* pCharacteristic) {
+        String value = pCharacteristic->getValue();
+        if (value.length() > 0) {
+            esp_ble_gap_set_device_name(value.c_str());
+            Serial.println("Changed device name");
+        }
+    }
+};
 
 class MyCallbacks : public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic* pCharacteristic) {
@@ -36,7 +48,6 @@ void setup() {
     Serial.begin(9600);
     // Init server
     BLEDevice::init(DEFAULT_DEVICE_NAME);
-    // esp_ble_gap_set_device_name(DEFAULT_DEVICE_NAME); // <-- Method to change device name I think
     BLEServer* pServer = BLEDevice::createServer();
     pServer->setCallbacks(new ServerCallBacks());
 
@@ -44,17 +55,30 @@ void setup() {
     BLEService* pService = pServer->createService(SERVICE_UUID);
 
     // Create characteristic in service
-    BLECharacteristic* pCharacteristic = pService->createCharacteristic(
-                                           CHARACTERISTIC_UUID,
+    BLECharacteristic* nameCharacteristic = pService->createCharacteristic(
+                                           NAME_CHARACTERISTIC_UUID,
                                            BLECharacteristic::PROPERTY_READ |
                                            BLECharacteristic::PROPERTY_WRITE
                                          );
-    pCharacteristic->setCallbacks(new MyCallbacks());
-    pCharacteristic->setValue("Hello World");
+    nameCharacteristic->setCallbacks(new NameCallback());
+    nameCharacteristic->setValue(DEFAULT_DEVICE_NAME);
     // Add a descriptor to the characteristic
-    BLEDescriptor* pDescriptor = new BLEDescriptor(DESCRIPTOR_UUID);
-    pDescriptor->setValue("Distance");
-    pCharacteristic->addDescriptor(pDescriptor);
+    BLEDescriptor* nameDescriptor = new BLEDescriptor(NAME_DESCRIPTOR_UUID);
+    nameDescriptor->setValue("Name");
+    nameCharacteristic->addDescriptor(nameDescriptor);
+
+    // Create characteristic in service
+    BLECharacteristic* distanceCharacteristic = pService->createCharacteristic(
+                                           DISTANCE_CHARACTERISTIC_UUID,
+                                           BLECharacteristic::PROPERTY_READ |
+                                           BLECharacteristic::PROPERTY_WRITE
+                                         );
+    distanceCharacteristic->setCallbacks(new MyCallbacks());
+    distanceCharacteristic->setValue("Hello World");
+    // Add a descriptor to the characteristic
+    BLEDescriptor* distanceDescriptor = new BLEDescriptor(DISTANCE_DESCRIPTOR_UUID);
+    distanceDescriptor->setValue("Distance");
+    distanceCharacteristic->addDescriptor(distanceDescriptor);
 
     // Start everything
     pService->start();
