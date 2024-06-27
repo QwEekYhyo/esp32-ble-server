0a86c7431b5edca513aa228f44b9784f778c9273
Logan Lucas <logan.lucas83@gmail.com>
2024-06-27 17:30:03 +0200

    binding brightness to NeoPixel ring

--- ble-server.ino
+++ ble-server.ino
@@ -1,4 +1,5 @@
 #include <esp_bt_device.h>
+#include <Adafruit_NeoPixel.h>
 
 #include "include/BLEServerManager.hpp"
 
@@ -6,7 +7,13 @@
 
 #define FREQUENCY 2 // in Hz
 
-#define DEFAULT_DEVICE_NAME "Cucklord V3"
+#define DEFAULT_DEVICE_NAME "Hirondelle Server"
+
+Adafruit_NeoPixel pixels(24, LED_PIN);
+
+uint8_t getSymmetrical(uint8_t pixel) {
+    return pixel == 0 ? 0 : 24 - pixel;
+}
 
 class NameCallbacks : public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic* pCharacteristic) {
@@ -23,7 +30,17 @@ class NameCallbacks : public BLECharacteristicCallbacks {
 class BrightnessCallbacks : public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic* pCharacteristic) {
         int value = pCharacteristic->getValue().toInt();
-        analogWrite(LED_PIN, value);
+        for (uint8_t pixel = 0; pixel < 13; pixel++) {
+            if (pixel <= value) {
+                pixels.setPixelColor(pixel, 255, 0, 0);
+                pixels.setPixelColor(getSymmetrical(pixel), 255, 0, 0);
+            } else {
+                pixels.setPixelColor(pixel, 0, 0, 0);
+                pixels.setPixelColor(getSymmetrical(pixel), 0, 0, 0);
+            }
+        }
+
+        pixels.show();
     }
 };
 
@@ -44,7 +61,13 @@ class DistanceCallbacks : public BLECharacteristicCallbacks {
 void setup() {
     // Arduino specific soy dev things
     Serial.begin(9600);
-    pinMode(LED_PIN, OUTPUT);
+    // NeoPixel
+    pixels.begin();
+    for (uint8_t pixel = 0; pixel < 24; pixel++) {
+        pixels.setPixelColor(pixel, 0, 0, 0);
+    }
+    pixels.show();
+
 
     BLEServerManager server(DEFAULT_DEVICE_NAME);
 
